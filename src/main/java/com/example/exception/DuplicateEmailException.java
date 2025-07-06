5841458a0997175efd6645af4bd6291b63b8c212
dldb-chamchi <dladkfl49@gmail.com>
2025-07-06 21:58:44 +0900

    feat: 커스텀 예외 메시지 생성

---
+++ src/main/java/com/example/exception/DuplicateEmailException.java
@@ -0,0 +1,5 @@
+package com.example.exception;
+
+public class DuplicateEmailException extends RuntimeException {
+    public DuplicateEmailException(String message) { super(message); }
+}
---
+++ src/main/java/com/example/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.example.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+    public ErrorResponse(int status, String message) {
+        this.status  = status;
+        this.message = message;
+    }
+}
