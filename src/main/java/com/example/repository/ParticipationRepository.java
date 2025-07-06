65c61d9cc7ac3b6799d3ee76f948a5a70e6a6a8e
dldb-chamchi <dladkfl49@gmail.com>
2025-07-06 18:48:24 +0900

    feat: Participation 레포지토리 생성

---
+++ src/main/java/com/example/repository/ParticipationRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import com.example.model.Participation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+    List<Participation> findByGroupId(Long groupId);
+    List<Participation> findByMemberId(Long memberId);
+}
