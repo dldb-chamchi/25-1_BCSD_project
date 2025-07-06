a3b8c4db3ec6d4d3316bc8063f654ad27e6aee38
dldb-chamchi <dladkfl49@gmail.com>
2025-07-06 19:08:27 +0900

    feat: PostComment 레포지토리 생성

---
+++ src/main/java/com/example/repository/PostCommentRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import com.example.model.PostComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
+    List<PostComment> findByPostId(Long postId);
+    List<PostComment> findByMemberId(Long memberId);
+}
