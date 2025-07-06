dd2488f061e175281421010dff6d69b0f1ff4106
dldb-chamchi <dladkfl49@gmail.com>
2025-07-06 19:09:19 +0900

    feat: PostComment DTO (request,response) 생성

---
+++ src/main/java/com/example/dto/response/CommentResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentResponseDto {
+    private Long id;
+    private Long memberId;
+    private String content;
+    private LocalDateTime createdAt;
+}
