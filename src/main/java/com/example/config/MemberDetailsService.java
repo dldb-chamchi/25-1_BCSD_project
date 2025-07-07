c38941a9dd5279061836e2f92f4d44f68c527c59
dldb-chamchi <dladkfl49@gmail.com>
2025-07-07 14:49:13 +0900

    feat: MemberDetailsService 생성

---
+++ src/main/java/com/example/config/MemberDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.config;
+
+import com.example.model.Member;
+import com.example.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member m = memberRepo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + email));
+        return User.builder()
+                .username(m.getEmail())
+                .password(m.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
