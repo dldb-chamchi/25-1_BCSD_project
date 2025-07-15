b5d1009281927ae54aff6bb7458d847488a5cd3e
dldb-chamchi <dladkfl49@gmail.com>
2025-07-15 15:53:22 +0900

    feat: 에러 코드 추가

--- src/main/java/com/example/exception/errorCode/GroupErrorCode.java
+++ src/main/java/com/example/exception/errorCode/GroupErrorCode.java
@@ -12,7 +12,8 @@ public enum GroupErrorCode implements ErrorCode {
     HOST_ONLY_GROUP_DELETE(HttpStatus.BAD_REQUEST, "그룹은 호스트만 삭제할 수 있습니다"),
     NOT_DELETE_WITH_PARTICIPATION(HttpStatus.BAD_REQUEST, "참여자가 남아있는 그룹은 삭제할 수 없습니다"),
     NOT_DELETE_WITH_POST(HttpStatus.BAD_REQUEST, "게시글이 존재하는 그룹은 삭제할 수 없습니다"),
-    NOT_VALID_STATUS(HttpStatus.BAD_REQUEST, "유효한 상태가 아닙니다")
+    NOT_VALID_STATUS(HttpStatus.BAD_REQUEST, "유효한 상태가 아닙니다(OPEN/CLOSED)"),
+    NOT_VALID_OPEN(HttpStatus.BAD_REQUEST, "현재 시간이 마감기한보다 미래이므로 OPEN으로 변경이 불가능합니다")
     ;
 
     private final HttpStatus httpStatus;
