bab871ea4d476bcdedbf8cc11d5872b69a48655f
dldb-chamchi <dladkfl49@gmail.com>
2025-07-17 11:04:56 +0900

    feat: valid 어노테이션 추가

--- src/main/java/com/example/auth/LoginRequestDto.java
+++ src/main/java/com/example/auth/LoginRequestDto.java
@@ -1,3 +1,13 @@
 package com.example.auth;
 
-public record LoginRequestDto(String email, String password) { }
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequestDto(
+        @NotBlank(message = "email은 비어 있을 수 없습니다")
+        String email,
+
+        @NotBlank(message = "password는 비어 있을 수 없습니다")
+        String password
+) {
+
+}
